66a5921d690c6e12758f84c1191fe2589ad0ecde
mutai-gilbert <mutaigilbert85@gmail.com>
2022-11-18 16:55:56 +0300

    Made new changes to  app.js file to add the features on email validation

--- app.js
+++ app.js
@@ -282,3 +282,25 @@ function popAllProject() {
 window.onload = () => {
   popAllProject();
 };
+
+/* Validate form */
+function onSubmit(event) {
+  const validate = document.getElementById('email');
+  const errorMessage = document.getElementById('error');
+  const email = validate.value;
+
+  if (email.toLowerCase() !== email) {
+    event.preventDefault();
+    validate.classList.add('invalid');
+    errorMessage.classList.add('error');
+    errorMessage.innerText = 'Email should be lowercase!';
+  } else {
+    validate.classList.remove('invalid');
+    errorMessage.classList.remove('error');
+  }
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', onSubmit);
+
+// Here is where you can remove the comments
